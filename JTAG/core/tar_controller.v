19079983c8d45bf77f41a9b0185a2ef4a0c0cef9
dymons <dmitriy.emelyanov.de@gmail.com>
2018-09-28 11:21:27 +0300

    JTAG

--- JTAG/core/tar_controller.v
+++ JTAG/core/tar_controller.v
@@ -1,10 +1,10 @@
 module tar_controller
 (
-	input 	TDI
-,	input	TDO
-,	input	TMS
-,	input	TCK
-,	input	TRST
+    input   TDI
+,   input   TDO
+,   input   TMS
+,   input   TCK
+,   input   TRST
 );
 
 // TAP controller state diagram / Specifications
@@ -27,74 +27,74 @@ localparam STATE_EXIT2_IR         = 4'b1110;
 localparam STATE_UPDATE_IR        = 4'b1111;
 
 always @(negedge TRST) begin
-	state <= STATE_TEST_LOGIC_RESET;
+    state <= STATE_TEST_LOGIC_RESET;
 end
 
 always @(posedge TCK) begin
-	case(state)
-		STATE_TEST_LOGIC_RESET: begin
-			if (TMS) begin state <= STATE_TEST_LOGIC_RESET; end
-			else     begin state <= STATE_RUN_TEST_IDLE;    end	
-		end
-		STATE_RUN_TEST_IDLE: begin
-			if (TMS) begin state <= STATE_SELECT_DR_SCAN;   end
-			else     begin state <= STATE_RUN_TEST_IDLE;    end	
-		end
-		STATE_SELECT_DR_SCAN: begin
-			if (TMS) begin state <= STATE_SELECT_IR_SCAN;   end
-			else     begin state <= STATE_CAPTURE_DR;       end	
-		end
-		STATE_CAPTURE_DR: begin
-			if (TMS) begin state <= STATE_EXIT1_DR;         end
-			else     begin state <= STATE_SHIFT_DR;         end
-		end
-		STATE_SHIFT_DR: begin
-			if (TMS) begin state <= STATE_EXIT1_DR;         end
-			else     begin state <= STATE_SHIFT_DR;         end
-		end
-		STATE_EXIT1_DR: begin
-			if (TMS) begin state <= STATE_UPDATE_DR;        end
-			else     begin state <= STATE_PAUSE_DR;         end
-		end
-		STATE_PAUSE_DR: begin
-			if (TMS) begin state <= STATE_EXIT2_DR;         end
-			else     begin state <= STATE_PAUSE_DR;         end
-		end
-		STATE_EXIT2_DR: begin
-			if (TMS) begin state <= STATE_UPDATE_DR;        end
-		end
-		STATE_UPDATE_DR: begin
-			if (TMS) begin state <= STATE_SELECT_DR_SCAN;   end
-			else     begin state <= STATE_RUN_TEST_IDLE;    end
-		end
-		STATE_SELECT_IR_SCAN: begin
-			if (TMS) begin state <= STATE_TEST_LOGIC_RESET; end
-			else     begin state <= STATE_CAPTURE_IR;       end	
-		end
-		STATE_CAPTURE_IR: begin
-			if (TMS) begin state <= STATE_EXIT1_IR;         end
-			else     begin state <= STATE_SHIFT_IR;         end
-		end
-		STATE_SHIFT_IR: begin
-			if (TMS) begin state <= STATE_EXIT1_IR;         end
-			else     begin state <= STATE_SHIFT_IR;         end
-		end
-		STATE_EXIT1_IR: begin
-			if (TMS) begin state <= STATE_UPDATE_IR;        end
-			else     begin state <= STATE_PAUSE_IR;         end
-		end
-		STATE_PAUSE_IR: begin
-			if (TMS) begin state <= STATE_EXIT2_IR;         end
-			else     begin state <= STATE_PAUSE_IR;         end
-		end
-		STATE_EXIT2_IR: begin
-			if (TMS) begin state <= STATE_UPDATE_IR;        end
-		end
-		STATE_UPDATE_IR: begin
-			if (TMS) begin state <= STATE_SELECT_DR_SCAN;   end
-			else     begin state <= STATE_RUN_TEST_IDLE;    end
-		end
-	endcase
+    case(state)
+        STATE_TEST_LOGIC_RESET: begin
+            if (TMS) begin state <= STATE_TEST_LOGIC_RESET; end
+            else     begin state <= STATE_RUN_TEST_IDLE;    end 
+        end
+        STATE_RUN_TEST_IDLE: begin
+            if (TMS) begin state <= STATE_SELECT_DR_SCAN;   end
+            else     begin state <= STATE_RUN_TEST_IDLE;    end 
+        end
+        STATE_SELECT_DR_SCAN: begin
+            if (TMS) begin state <= STATE_SELECT_IR_SCAN;   end
+            else     begin state <= STATE_CAPTURE_DR;       end 
+        end
+        STATE_CAPTURE_DR: begin
+            if (TMS) begin state <= STATE_EXIT1_DR;         end
+            else     begin state <= STATE_SHIFT_DR;         end
+        end
+        STATE_SHIFT_DR: begin
+            if (TMS) begin state <= STATE_EXIT1_DR;         end
+            else     begin state <= STATE_SHIFT_DR;         end
+        end
+        STATE_EXIT1_DR: begin
+            if (TMS) begin state <= STATE_UPDATE_DR;        end
+            else     begin state <= STATE_PAUSE_DR;         end
+        end
+        STATE_PAUSE_DR: begin
+            if (TMS) begin state <= STATE_EXIT2_DR;         end
+            else     begin state <= STATE_PAUSE_DR;         end
+        end
+        STATE_EXIT2_DR: begin
+            if (TMS) begin state <= STATE_UPDATE_DR;        end
+        end
+        STATE_UPDATE_DR: begin
+            if (TMS) begin state <= STATE_SELECT_DR_SCAN;   end
+            else     begin state <= STATE_RUN_TEST_IDLE;    end
+        end
+        STATE_SELECT_IR_SCAN: begin
+            if (TMS) begin state <= STATE_TEST_LOGIC_RESET; end
+            else     begin state <= STATE_CAPTURE_IR;       end 
+        end
+        STATE_CAPTURE_IR: begin
+            if (TMS) begin state <= STATE_EXIT1_IR;         end
+            else     begin state <= STATE_SHIFT_IR;         end
+        end
+        STATE_SHIFT_IR: begin
+            if (TMS) begin state <= STATE_EXIT1_IR;         end
+            else     begin state <= STATE_SHIFT_IR;         end
+        end
+        STATE_EXIT1_IR: begin
+            if (TMS) begin state <= STATE_UPDATE_IR;        end
+            else     begin state <= STATE_PAUSE_IR;         end
+        end
+        STATE_PAUSE_IR: begin
+            if (TMS) begin state <= STATE_EXIT2_IR;         end
+            else     begin state <= STATE_PAUSE_IR;         end
+        end
+        STATE_EXIT2_IR: begin
+            if (TMS) begin state <= STATE_UPDATE_IR;        end
+        end
+        STATE_UPDATE_IR: begin
+            if (TMS) begin state <= STATE_SELECT_DR_SCAN;   end
+            else     begin state <= STATE_RUN_TEST_IDLE;    end
+        end
+    endcase
 end
 
 endmodule
