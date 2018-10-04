59c99073253f7f5364612b932b125cc0b3a03fb1
dymons <dmitriy.emelyanov.de@gmail.com>
2018-10-04 19:33:13 +0300

    update

--- JTAG/core/dr.v
+++ JTAG/core/dr.v
@@ -1,32 +1,32 @@
 module dr
 (
     input            rst
-,	input			 TCK
-,	input			 TDI
-,	input	         UPDATEDR
-,	input	         SHIFTDR
-,	input			 CAPTUREDR
+,   input            TCK
+,   input            TDI
+,   input            UPDATEDR
+,   input            SHIFTDR
+,   input            CAPTUREDR
 ,   output reg [7:0] BSR
-,	output reg       BSR_TDO
-,	input            BYPASS_SELECT
-,	input            SAMPLE_SELECT
-,	input            EXTEST_SELECT
-,	input            INTEST_SELECT
-,	input            RUNBIST_SELECT
-,	input            CLAMP_SELECT
-,	input            IDCODE_SELECT
-,	input            USERCODE_SELECT
-,	input            HIGHZ_SELECT
+,   output reg       BSR_TDO
+,   input            BYPASS_SELECT
+,   input            SAMPLE_SELECT
+,   input            EXTEST_SELECT
+,   input            INTEST_SELECT
+,   input            RUNBIST_SELECT
+,   input            CLAMP_SELECT
+,   input            IDCODE_SELECT
+,   input            USERCODE_SELECT
+,   input            HIGHZ_SELECT
 );
 
 reg [7:0] device_ID_register = 8'hA1;
 
 always @ (posedge TCK) begin
-	if (IDCODE_SELECT && SHIFTDR) begin
-		BSR <= {TDI,BSR[7:1]};
-	end else begin
-		BSR <= device_ID_register;
-	end
+    if (IDCODE_SELECT && SHIFTDR) begin
+        BSR <= {TDI,BSR[7:1]};
+    end else begin
+        BSR <= device_ID_register;
+    end
 end
 
 always @ (negedge TCK) begin
