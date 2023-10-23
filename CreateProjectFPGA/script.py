ae7788785d83451acb4a4d646f16b09cfa36984a
Sourcery AI <>
2023-10-23 23:25:19 +0000

    'Refactored by Sourcery'

--- CreateProjectFPGA/script.py
+++ CreateProjectFPGA/script.py
@@ -39,9 +39,9 @@ def clearProject():
     for disc in directory:
         if os.path.exists(disc):
             shutil.rmtree(disc)
-            print ("Папка %s удалена" % disc)
+            print(f"Папка {disc} удалена")
         else:
-            print ("Папки %s нет в дирректроии" % disc)
+            print(f"Папки {disc} нет в дирректроии")
             sys.exit (1)
 
 def createFolderFile(): 
@@ -58,22 +58,20 @@ def createFolderFile():
         try:
             if os.path.exists(dirc):
                 shutil.rmtree(dirc)
-                os.mkdir(dirc)
-            else:
-                os.mkdir(dirc)
+            os.mkdir(dirc)
         except OSError:
-            print ("Создать директорию %s не удалось" % dirc)
+            print(f"Создать директорию {dirc} не удалось")
         else:
-            print ("Директория %s создана" % dirc)
+            print(f"Директория {dirc} создана")
 
     # Создаем файлы
     for dirc, file in files.items():
 
         for item in file:
-            if (dirc == directory[0] or dirc == directory[1]):
-                tar = open(dirc + '/' + item + '.v', 'w')
+            if dirc in [directory[0], directory[1]]:
+                tar = open(f'{dirc}/{item}.v', 'w')
             else:
-                tar = open(dirc + '/' + item, 'w')
+                tar = open(f'{dirc}/{item}', 'w')
             tar.close()
 
     # Добавляем шаблон для Verilog файлов
@@ -88,67 +86,83 @@ def templateVerilogFile():
     for dirc, file in files.items():
         if (dirc ==  directory[0]):
             for item in file:
-                tar = open(dirc + '/' + item + '.v', 'w')
-                tar.write("module " + item + "\n#(\n    parameter DEFAULT = 32\n)\n("
-                      + "\n     input                   clk"
-                      + "\n,    input                   rst"
-                      + "\n,    input   [DEFAULT - 1:0] data_in"
-                      + "\n,    output  [DEFAULT - 1:0] data_out"
-                      + "\n);"
-                      + "\n\nalways @(posedge clk) begin"
-                      + "\n if ( rst ) begin\n      \n  end else begin\n        \n  end"
-                      + "\nend\n\nendmodule")
-                tar.close()
+                with open(f'{dirc}/{item}.v', 'w') as tar:
+                    tar.write(
+                        (
+                            (
+                                (
+                                    (
+                                        (
+                                            (
+                                                (
+                                                    (
+                                                        f"module {item}"
+                                                        + "\n#(\n    parameter DEFAULT = 32\n)\n("
+                                                        + "\n     input                   clk"
+                                                    )
+                                                    + "\n,    input                   rst"
+                                                )
+                                                + "\n,    input   [DEFAULT - 1:0] data_in"
+                                            )
+                                            + "\n,    output  [DEFAULT - 1:0] data_out"
+                                        )
+                                        + "\n);"
+                                    )
+                                    + "\n\nalways @(posedge clk) begin"
+                                )
+                                + "\n if ( rst ) begin\n      \n  end else begin\n        \n  end"
+                            )
+                            + "\nend\n\nendmodule"
+                        )
+                    )
         elif (dirc ==  directory[1]):
             for item in file:
-                tar = open(dirc + '/' + item + '.v', 'w')
-                tar.write("`timescale 1 ns / 1 ns\nmodule " + item 
-                    + "\n#(\n   parameter DEFAULT = 32\n);"
-                    + "\n\nreg clk, rst;"
-                    + "\n\n" + item.replace("_tb", "") + " #("
-                    + "\n   .DEFAULT( DEFAULT ) "
-                    + "\n) " + item.replace("_tb", "_sample")
-                    + "\n( .clk(clk)\n, .rst(rst)\n);"
-                    + "\n\nalways begin\n   #5  clk <= ~clk; // 200MHz\nend"
-                    + "\n\ninitial begin"
-                    + "\n   clk <= 0; @(posedge clk);"
-                    + "\n   rst <= 1; @(posedge clk);"
-                    + "\n   rst <= 0; @(posedge clk);\nend"
-                    + "\n\ninitial begin\n  // code... \n    @(negedge rst);\n    $finish;\nend"
-                    + "\n\ninitial begin\n  $dumpfile(\"" + item + ".vcd\");"
-                    + "\n   $dumpvars(-1, " + item + ");"
-                    + "\nend\n\nendmodule")
-                tar.close()
+                with open(f'{dirc}/{item}.v', 'w') as tar:
+                    tar.write("`timescale 1 ns / 1 ns\nmodule " + item 
+                        + "\n#(\n   parameter DEFAULT = 32\n);"
+                        + "\n\nreg clk, rst;"
+                        + "\n\n" + item.replace("_tb", "") + " #("
+                        + "\n   .DEFAULT( DEFAULT ) "
+                        + "\n) " + item.replace("_tb", "_sample")
+                        + "\n( .clk(clk)\n, .rst(rst)\n);"
+                        + "\n\nalways begin\n   #5  clk <= ~clk; // 200MHz\nend"
+                        + "\n\ninitial begin"
+                        + "\n   clk <= 0; @(posedge clk);"
+                        + "\n   rst <= 1; @(posedge clk);"
+                        + "\n   rst <= 0; @(posedge clk);\nend"
+                        + "\n\ninitial begin\n  // code... \n    @(negedge rst);\n    $finish;\nend"
+                        + "\n\ninitial begin\n  $dumpfile(\"" + item + ".vcd\");"
+                        + "\n   $dumpvars(-1, " + item + ");"
+                        + "\nend\n\nendmodule")
 
 
 def templateMakefileFile():
-    makefile = open('script/Makefile', 'w')
+    with open('script/Makefile', 'w') as makefile:
+        for dirc, file in files.items():
+            if (dirc ==  directory[0]):
+                for item in file:
+                    if (item == files["core"][2]):
+                        makefile.write("FILE_" + files["core"][2] + " =")
+                        for dirc, file in files.items():
+                            if (dirc ==  directory[0]):
+                                for item in file:
+                                    makefile.write(f" ../core/{item}.v")
+                        makefile.write(f" ../{directory[1]}/" + files["core"][2] + "_tb.v" + "\n")
 
-    for dirc, file in files.items():
-        if (dirc ==  directory[0]):
-            for item in file:
-            	if (item == files["core"][2]):
-            		makefile.write("FILE_" + files["core"][2] + " =")
-            		for dirc, file in files.items():
-            			if (dirc ==  directory[0]):
-            				for item in file:
-            					makefile.write(" ../core/" + item + ".v")
-            		makefile.write(" ../" + directory[1] + "/" + files["core"][2] + "_tb.v" +  "\n")
+                    else:
+                        makefile.write(
+                            f"FILE_{item} = ../core/{item}.v ../{directory[1]}/{item}_tb.v"
+                            + "\n"
+                        )
 
-            	else:
-                	makefile.write("FILE_" + item + " = ../core/" + item + ".v ../" + directory[1] + "/" + item + "_tb.v" + "\n")
+        makefile.write("\n")
 
-    makefile.write("\n")
-
-    for dirc, file in files.items():
-        if (dirc ==  directory[0]):
-            for item in file:
-            	 makefile.write(item + "_compile:\n	iverilog -o " + item + " $(FILE_" + item + ")\n\n")
-            	 makefile.write(item + "_vvp" + ":\n	vvp " + item + "\n\n")
-            	 makefile.write(item + "_gtk" + ":\n	gtkwave " + item + "_tb.vcd\n\n")
-
-
-    makefile.close()
+        for dirc, file in files.items():
+            if (dirc ==  directory[0]):
+                for item in file:
+                    makefile.write(item + "_compile:\n	iverilog -o " + item + " $(FILE_" + item + ")\n\n")
+                    makefile.write(f"{item}_vvp" + ":\n	vvp " + item + "\n\n")
+                    makefile.write(f"{item}_gtk" + ":\n	gtkwave " + item + "_tb.vcd\n\n")
 
 def templateTclFile():
     pass
@@ -158,22 +172,22 @@ def main():
     if len (sys.argv) == 1:
         print("Ошибка: нет ключей. Выберете из предложенного: ")
         for arg, name in args.items():
-            print("--" + arg + " - " + name)
+            print(f"--{arg} - {name}")
     else:
         if len (sys.argv) > 2:
             print ("Ошибка. Введите только один параметр: ")
             for arg, name in args.items():
-                print("--" + arg + " - " + name)
+                print(f"--{arg} - {name}")
             sys.exit (1)
 
-        if   (sys.argv[1] == "--init"):   createFolderFile()
+        if (sys.argv[1] == "--init"):   createFolderFile()
         elif (sys.argv[1] == "--start"):  runFunctionModel()
         elif (sys.argv[1] == "--update"): updateModel()
         elif (sys.argv[1] == "--clear"):  clearProject()
         else: 
             print ("Не знаю такого =( Воспользуйся из предложенного: ")
             for arg, name in args.items():
-                print("--" + arg + " - " + name)
+                print(f"--{arg} - {name}")
 
 if __name__ == '__main__':
     main()
